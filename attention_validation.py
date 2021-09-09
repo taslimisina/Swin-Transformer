f8f94f143698d3aaa11862b2ad430d45845cfb34
taslimisina <taslimisina@yahoo.com>
2021-09-09 11:44:52 +0430

    debug

--- attention_validation.py
+++ attention_validation.py
@@ -198,6 +198,8 @@ def get_final_attention(layers_all_attn_weights, window_size=7, resolution=224):
     for i in range(1, len(xs)):
         print("i:", i)
         for j in range(finalx.shape[0]):
+            print("finalx[j].shape:", finalx[j].shape)
+            print("xs[i][j].shape:", xs[i][j].shape)
             finalx[j] = finalx[j] * xs[i][j]
     mx = torch.max(finalx.view(finalx.shape[0], -1), dim=1)[0]
     scale = 1 / mx
